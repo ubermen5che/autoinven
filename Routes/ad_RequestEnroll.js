b2d3da6f2384edfcab593b0142fa870fbe654d42
ubermen5che <ubermen5ch1308@gmail.com>
2021-02-04 15:44:22 +0900

    + admin에서 Provider 계정정보를 확인할 수 있기 위해 json형태로 정보를 넘겨줄 수 있도록 code 추가.

--- Routes/ad_RequestEnroll.js
+++ Routes/ad_RequestEnroll.js
@@ -1,6 +1,7 @@
 exports.RequestForEnroll = function (req, res,app,db) {
   var items="{";
-  let results = db.query('SELECT * from RequestForEnroll');
+  let results = db.query('select * from RequestForEnroll, Member, Warehouse where RequestForEnroll.providerID=Member.memberID and RequestForEnroll.warehouseID=Warehouse.warehouseID');
+  console.log(results);
   if(results.length > 0) {
       var step;
       for(step =0;step<results.length;step++){
@@ -10,8 +11,14 @@ exports.RequestForEnroll = function (req, res,app,db) {
               "\"reqDate\" :\""+ results[step].reqDate+"\","+
               "\"reqType\" :\"" + results[step].reqType+"\","+
               "\"warehouseID\" :"+ results[step].warehouseID+","+
-              "\"providerID\" :\""+ results[step].providerID  +"\","+
-              "\"logID\" :"+ results[step].logID+
+              "\"providerID\" :\""+ results[step].providerID+"\","+
+              "\"national\" :\""+ results[step].national +"\","+
+              "\"CA\" :\""+ results[step].CA +"\","+
+              "\"CN\" :\""+ results[step].CN +"\","+
+              "\"CCN\" :\""+ results[step].CCN +"\","+
+              "\"address\" :\""+ results[step].address +"\","+
+              "\"name\" :\""+ results[step].name +"\","+
+              "\"floorArea\" :"+ results[step].floorArea+
           "}";
           items+=obj;
           if(step+1<results.length)items+=","
@@ -24,11 +31,13 @@ exports.RequestForEnroll = function (req, res,app,db) {
 exports.withAnswer = function(req,res,app,db){
   var providerID = req.body.providerID;
   var reqID = req.body.reqID;
+  var warehouseID = req.body.warehouseID;
   var reqType = req.body.reqType;
   var answer = req.body.answer;
   var mysql = require('mysql');
   var connection = mysql.createConnection(require('../Module/db').info);
   connection.connect();
+  console.log('reqbody :' + req.body);
   if(answer=="Approve"){
       if(reqType=="ReqEnrollPV"){
         let reqResult = db.query('SELECT * from Provider');
@@ -45,10 +54,10 @@ exports.withAnswer = function(req,res,app,db){
           }
           else{
             connection.query("DELETE FROM RequestForEnroll WHERE reqID ="+reqID,function (error, results, fields) {
-              if(error){res.send(false);connection.end();}
+              if(error){console.log('Error at Delete from RQFEnroll');res.send(false);connection.end();}
               else{
                 connection.query('INSERT INTO EnrolledWarehouse SET ?',{'warehouseID':info.warehouseID,'logID':11111111},function (error, results, fields) {
-                  if(error){res.send(false);connection.end();}
+                  if(error){console.log('Error at Insert EnrollWarehouse' + error);res.send(false);connection.end();}
                   else{
                     res.send(true);
                     connection.end();
