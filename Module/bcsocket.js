5b4144b6c7f4e2f88822d4716c89c178b50b5927
ubermen5che <ubermen5ch1308@gmail.com>
2021-02-04 15:54:31 +0900

    * 블록체인 모듈 주석처리. (사용하지 않음)

--- Module/bcsocket.js
+++ Module/bcsocket.js
@@ -1,4 +1,4 @@
-const net = require('net');
+/*const net = require('net');
 function getConnection(connName){
       var client = net.connect({port:7777,host:'localhost'},function(){
         console.log(connName+"Connected: ");
@@ -13,3 +13,5 @@ var client = getConnection("WEB_SERVER");
 module.exports = {
     socket : client
 }
+*/
+// autoinven 실서비스에서는 블록체인 기능을 뺏음. 사용하지않으므로 주석처리하였음.
