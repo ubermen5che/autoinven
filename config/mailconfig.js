97dc9b581609153b13e0b7b88e4439ed5e6b1f55
ubermen5che <ubermen5ch1308@gmail.com>
2021-02-04 14:06:07 +0900

    * modified mailing configuration.

--- config/mailconfig.js
+++ config/mailconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    mailhost : 'smtp.naver.com',
-    mailid : process.env.MAILID || 'kcl5363@naver.com',
-    mailpassword : process.env.MAILPASSWORD || 'ljh5363019892!',
-    mailservice : process.MAILSERVICE || 'Naver'
+    mailhost : 'smtp.worksmobile.com',
+    mailid : process.env.MAILID || 'service@autoingroup.com',
+    mailpassword : process.env.MAILPASSWORD || 'autoin2021$',
+    mailservice : process.MAILSERVICE || 'Lineworks'
 };
