4bcd2e83f8d9bf60200ae621338666c598f41008
ubermen5che <ubermen5ch1308@gmail.com>
2021-02-05 13:55:33 +0900

    * production 설정으로 변경.

--- Module/db.js
+++ Module/db.js
@@ -2,9 +2,9 @@ const mysql = require('sync-mysql');
 
 var db_info = {
     host : process.env.MYSQL_HOST_IP, //docker mysql container의 ip주소로 변경해주어야함.
-    user : 'user',
+    user : 'autoin',
     port : 3306,
-    password : 'password',
+    password : 'autoin102030',
     database : 'autoinven',
 };
 
